a10f177a8be3737ea05e5d06b7ceb8e9fef7561e
Eduardo Picoral Sarandy Machado Filho <eduardo.picoral@gmail.com>
2017-02-03 16:18:03 -0200

    finalizações dos templates

--- organizer/views.py
+++ organizer/views.py
@@ -1,22 +1,14 @@
-from django.http import HttpResponse 
+from django.http.response import HttpResponse 
+from django.template import loader, Context
+
 from .models import Tag
 
 def homepage(request):
-	tag_list = Tag.objects.all()
-	output = "<html>\n"
-	output += "<head>\n"
-	output += " <title>"
-	output += "Don't do This!</title>\n"
-	output += "</head> \n"
-	output += "<body>\n"
-	output += " <ul>\n"
-	for tag in tag_list:
-		output += " <li>"
-		output += tag.name.title() 
-		output += "</li>\n"
-	output += " </ul>\n"
-	output += "</body>\n"
-	output += "</html>\n"
-	return HttpResponse(output)
+    tag_list = Tag.objects.all()
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
+    return HttpResponse(output)
 
 # Create your views here.
+        
